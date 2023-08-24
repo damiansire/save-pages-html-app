b82af659d26cb48f640dd3024cbc9a285b63f157
Damian Sire <damiansire@hotmail.com>
2023-08-24 04:49:07 -0300

    libs: add eslint and axios

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // ... Otras configuraciones ...
+    plugins: ['@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        // ... Otras configuraciones ...
+    ],
+    // ... Otras configuraciones ...
+};
